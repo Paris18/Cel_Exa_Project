9796f1b1464f8e9d5193fd8b9c92e9d36b6c7bcd
rajunadaf570 <rajunadaf570@gmail.com>
2019-08-06 17:28:27 +0530

    Requirement Update

--- AdNube/htmldowloader/htmldowloader/settings.py
+++ AdNube/htmldowloader/htmldowloader/settings.py
@@ -134,7 +134,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'pariskamal8@gmail.com'
-EMAIL_HOST_PASSWORD = 'shri1008'
+EMAIL_HOST_PASSWORD = 'xxxxxxxx'
 
 
 # CELERY STUFF
